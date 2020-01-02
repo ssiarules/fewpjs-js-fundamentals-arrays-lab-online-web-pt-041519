619bcdb44d7049eee07aee5c625996420e9ac1ca
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-02 23:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garield"]
+const prepend = ["Milo", "Otis", "Garield"]
+const removeLast = ["Milo", "Otis", "Garield"]
+const removeFirst = ["Milo", "Otis", "Garield"]
